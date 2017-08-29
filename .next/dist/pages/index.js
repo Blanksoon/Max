1b69e76b30feeffe11975ef9aad04d7cecc6a7bf
Blanksoon <farm1771@gmail.com>
2017-08-29 14:16:39 +0700

    first commit

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _link = require('next/dist/lib/link.js');
+
+var _link2 = _interopRequireDefault(_link);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = '/Users/farmoz/Desktop/MaxMuayThai/front-end/MaxFront/pages/index.js?entry';
+// This is the Link API
+
+
+var Index = function Index() {
+  return _react2.default.createElement('div', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, _react2.default.createElement('p', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    }
+  }, 'Hello '));
+};
+
+exports.default = Index;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// This is the Link API
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <p>Hello </p>
+  </div>
+)
+
+export default Index
