f0d8634eb8061bdfd97110958a4e3da1057bc1fb
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-09-30 20:56:32 +0700

    Improve fb button style

--- components/login/FacebookLoginButton.js
+++ components/login/FacebookLoginButton.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import Cookies from 'universal-cookie'
 import FacebookLogin from 'react-facebook-login'
+import api from '../../api'
 
 const cookies = new Cookies()
 const Wrapper = styled.div`color: red;`
@@ -11,14 +12,8 @@ class FacebookLoginButton extends React.Component {
       provider_name: 'facebook',
       provider_data: response,
     }
-    var j = {
-      provider_name: 'facebook',
-      provider_data: response,
-    }
-    //j = JSON.stringify(j)
-    // console.log('providerData', j)
 
-    fetch('http://139.59.127.206:3001/fb-login', {
+    fetch(`${api.SERVER}/fb-login`, {
       method: 'POST',
       body: JSON.stringify(providerData),
       cors: 'cors',
@@ -43,21 +38,59 @@ class FacebookLoginButton extends React.Component {
           appId="1492448664168205"
           autoLoad={false}
           fields="name,email,id,gender,locale,age_range"
-          //cssClass="my-facebook-button-class"
-          textButton="f Log in with Facebook"
+          cssClass="loginBtn loginBtn--facebook"
+          textButton="Login with Facebook"
           callback={this.facebookResponse}
         />
-        <style jsx>
+        <style jsx global>
           {`
-            .button-right {
-              background-color: #3a559f;
+            /* Shared */
+            .loginBtn {
+              box-sizing: border-box;
+              position: relative;
+              /* width: 13em;  - apply for fixed size */
+              margin: 0.2em;
+              padding: 0 15px 0 46px;
               border: none;
-              color: white;
-              padding: 10px 30px;
-              text-align: center;
-              text-decoration: none;
-              display: inline-block;
-              font-size: 1em;
+              text-align: left;
+              line-height: 34px;
+              white-space: nowrap;
+              border-radius: 0.2em;
+              font-size: 16px;
+              color: #fff;
+            }
+            .loginBtn:before {
+              content: '';
+              box-sizing: border-box;
+              position: absolute;
+              top: 0;
+              left: 0;
+              width: 34px;
+              height: 100%;
+            }
+            .loginBtn:focus {
+              outline: none;
+            }
+            .loginBtn:active {
+              box-shadow: inset 0 0 0 32px rgba(0, 0, 0, 0.1);
+            }
+
+            /* Facebook */
+            .loginBtn--facebook {
+              background-color: #4c69ba;
+              background-image: linear-gradient(#4c69ba, #3b55a0);
+              /*font-family: "Helvetica neue", Helvetica Neue, Helvetica, Arial, sans-serif;*/
+              text-shadow: 0 -1px 0 #354c8c;
+            }
+            .loginBtn--facebook:before {
+              border-right: #364e92 1px solid;
+              background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_facebook.png')
+                6px 6px no-repeat;
+            }
+            .loginBtn--facebook:hover,
+            .loginBtn--facebook:focus {
+              background-color: #5b7bd5;
+              background-image: linear-gradient(#5b7bd5, #4864b1);
             }
           `}
         </style>
