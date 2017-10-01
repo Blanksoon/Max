083dea36af72fdf23419ac768cdb9c59932739ac
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2017-10-01 18:42:01 +0700

    Add live modules

--- redux/modules/live.js
+++ redux/modules/live.js
@@ -1,22 +1,21 @@
 import * as api from '../../api'
 
 // types
-const FETCH_VODS = 'FETCH_VODS'
-const FETCH_VODS_SUCCESS = 'FETCH_VODS_SUCCESS'
+const FETCH_LIVES = 'FETCH_LIVES'
+const FETCH_LIVES_SUCCESS = 'FETCH_LIVES_SUCCESS'
 
 // actions
-export const fetchVodsSuccess = vods => ({
-  type: FETCH_VODS_SUCCESS,
-  payload: vods,
+export const fetchLivesSuccess = lives => ({
+  type: FETCH_LIVES_SUCCESS,
+  payload: lives,
 })
 
-export const fetchVods = token => async dispatch => {
-  //console.log('token', token)
+export const fetchLives = token => async dispatch => {
   const url = `${api.SERVER}/vod`
   try {
     const json = await api.post(url, { token })
-    if (json.data.vods != undefined) {
-      dispatch(fetchVodsSuccess(json.data.vods))
+    if (json.data.lives != undefined) {
+      dispatch(fetchLivesSuccess(json.data.lives))
     }
   } catch (error) {
     console.log(error)
@@ -29,7 +28,7 @@ const initialState = {
 }
 const vodReducer = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_VODS_SUCCESS:
+    case FETCH_LIVES_SUCCESS:
       const vodById = {}
       if (Array.isArray(action.payload)) {
         action.payload.forEach(vod => {
--- redux/rootReducer.js
+++ redux/rootReducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
 import authReducer from './modules/auth'
 import cookieReducer from './modules/cookie'
+import liveReducer from './modules/live'
 import modalReducer from './modules/modal'
 import vodReducer from './modules/vod'
 
 const rootReducer = combineReducers({
   auth: authReducer,
   cookie: cookieReducer,
+  live: liveReducer,
   modal: modalReducer,
   vod: vodReducer,
 })
