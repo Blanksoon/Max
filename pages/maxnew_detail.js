01fd6763ebe840c94089c8f383f21bf3ab8c27bf
Blanksoon <farm1771@gmail.com>
2017-10-11 13:11:15 +0700

    Fix conflict in max_detail

--- pages/maxnew_detail.js
+++ pages/maxnew_detail.js
@@ -5,12 +5,8 @@ import { Flex, Box, Image, Text } from 'rebass'
 import Container from '../components/commons/Container'
 import Main from '../layouts/Main'
 import color from '../components/commons/vars'
-<<<<<<< HEAD
-import MaxTop from '../components/maxNews/MaxTop'
-=======
 import DetailLeft from '../components/maxNews/DetailLeft'
 import MaxButtom from '../components/maxNews/MaxButtom'
->>>>>>> ecda9b58bf35e8d9949a5f804ca732a69bfef501
 import Purchase from '../components/getTicket/Purchase'
 import withRedux from 'next-redux-wrapper'
 import { initStore } from '../redux/store'
@@ -37,16 +33,12 @@ class maxnew_detail extends React.Component {
           <NewModal />
           <Wrapper>
             <Container>
-<<<<<<< HEAD
-              <Box pt="7rem">{/* <MaxTop /> */}</Box>
-=======
               <Flex>
                 <Box w={8 / 12} pt="7rem">
                   <DetailLeft />
                 </Box>
                 <Box W={3 / 12} />
               </Flex>
->>>>>>> ecda9b58bf35e8d9949a5f804ca732a69bfef501
             </Container>
           </Wrapper>
         </Main>
