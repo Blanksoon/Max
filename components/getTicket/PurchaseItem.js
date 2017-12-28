63645582bc7eb28c143e55ba34371fda3fa1042f
Blanksoon <farm1771@gmail.com>
2017-12-29 00:31:16 +0700

    fix url api

--- components/getTicket/PurchaseItem.js
+++ components/getTicket/PurchaseItem.js
@@ -451,8 +451,8 @@ class PurchaseItem extends React.Component {
                       <StripeCheckout
                         token={this.onToken}
                         name={packagee}
-                        stripeKey="pk_test_qghYMOBiEuWIDjedt7DNPA0w" //dev
-                        //stripeKey="pk_live_trWuol5XDXULmz0mK9eWwihA" //live
+                        //stripeKey="pk_test_qghYMOBiEuWIDjedt7DNPA0w" //dev
+                        stripeKey="pk_live_trWuol5XDXULmz0mK9eWwihA" //live
                         email={this.props.auth.email}
                         amount={amo}
                         //allowRememberMe="false"
