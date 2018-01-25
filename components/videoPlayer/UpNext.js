70665a8ec0d7ada0790cf824ac522d1c1cf9a9e7
Blanksoon <farm1771@gmail.com>
2018-01-25 12:42:36 +0700

    fix responsive live-vod

--- components/videoPlayer/UpNext.js
+++ components/videoPlayer/UpNext.js
@@ -12,6 +12,7 @@ const Title = styled.div`
   margin-right: 0;
   font-weight: bold;
   ${media.phone`font-size: 1em;`};
+  ${media.iphone5`font-size: 0.9em;`};
 `
 
 const renderVideos = (lang, vod) => {
@@ -21,7 +22,7 @@ const renderVideos = (lang, vod) => {
   // Use splice on clone object, DONT MODIFY props
   for (let i = 0; i < rowCount && vod[i] != undefined; i++) {
     rowVideos.push(
-      <Box width={[1 / 4, 2 / 4, 1 / 4, 1 / 4, 1 / 4]} key={vod[i].id} px={2}>
+      <Box width={[2 / 4, 2 / 4, 1 / 4, 1 / 4, 1 / 4]} key={vod[i].id} px={2}>
         <ThumbnailVideo
           img={vod[i].thumbnailUrl}
           name={lang === 'en' ? vod[i].title_en : vod[i].title_th}
