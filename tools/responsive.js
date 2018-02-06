44e9d9ac6dc9694f711db887ec6f5224e7b90680
Blanksoon <farm1771@gmail.com>
2018-02-06 16:18:23 +0700

    Test respon

--- tools/responsive.js
+++ tools/responsive.js
@@ -2,22 +2,22 @@
 import { css } from 'styled-components'
 
 const sizes = {
-  iphone5: 325,
-  phone: 376,
-  ipad: 768,
-  ipadxl: 850,
-  ipadpro: 1024,
-  desktop: 1120,
+  iphone5: 376, //325,
+  phone: 376, //376,
+  ipad: 376, //768,
+  ipadxl: 376, //850,
+  ipadpro: 376, //1024,
+  desktop: 376, //1120,
 }
 
 export const theme = {
   breakpoints: [
     // min-width breakpoints in em
-    20.1, //iphone5
-    23.44, //phone
-    48.1, //ipad
-    64.1, //ipadpro
-    70.1, //desktop
+    23.44, // 20.1, //iphone5
+    23.44, // 23.44, //phone
+    23.44, // 48.1, //ipad
+    23.44, // 64.1, //ipadpro
+    23.44, // 70.1, //desktop
   ],
 }
 // iterate through the sizes and create a media template
