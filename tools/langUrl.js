41027d9595e276311af952a896a9a8c4ef888e30
Wasupol <wasupol.v@gmail.com>
2018-03-29 15:08:15 +0700

    Edit maxnews

--- tools/langUrl.js
+++ tools/langUrl.js
@@ -1,3 +1,3 @@
-export const langUrl = `http://localhost:8080/static/locales/` //dev
+// export const langUrl = `http://localhost:8080/static/locales/` //dev
 // export const langUrl = `https://beta.maxmuaythai.com/static/locales/` //staging
-// export const langUrl = `https://www.maxmuaythai.com/static/locales/` //production
+export const langUrl = `https://www.maxmuaythai.com/static/locales/` //production
